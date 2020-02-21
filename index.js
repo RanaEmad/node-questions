65a5f880378b37382d653180da50606d3a69f6d8
Rana Emad <rana3emad@gmail.com>
2020-02-21 15:39:25 +0200

    add redis connection to store data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let app= require("express")();
 let server= require("http").createServer(app);
 let io = require("socket.io")(server);
+let redis=require("redis").createClient();
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -9,10 +10,17 @@ app.get('/', function(req, res){
 io.on("connection", function(socket){
     console.log("A new user joined");
     socket.on("question",function(question){
-        socket.broadcast.emit("question",socket.name+" asked: "+question);
+        var text=socket.name+" asked: "+question;
+        socket.broadcast.emit("question",text);
+        redis.lpush("questions",text);
     })
     socket.on("join",function(name){
         socket.name=name;
+    });
+    redis.lrange("questions",0,-1,function(err,data){
+        data.reverse().forEach(function(question){
+            socket.emit("question",question);
+        });
     });
 });
 
