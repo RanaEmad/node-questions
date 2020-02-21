f475f32d438bcfff6699843e31b503687ffe1c68
Rana Emad <rana3emad@gmail.com>
2020-02-21 15:00:35 +0200

    listen on question submission

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ let app= require("express")();
 let server= require("http").createServer(app);
 let io = require("socket.io")(server);
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+
+io.on("connection", function(socket){
+    console.log("A new user joined");
+    socket.on("question",function(question){
+        socket.broadcast.emit("question",question);
+    })
+});
 
 server.listen(3000, function(){
     console.log("Listening on port 3000");
